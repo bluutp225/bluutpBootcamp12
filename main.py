5265cebb066de8c0a5ee9ae840e0cc65fd2924cd
bluutp225 <bluutp225@gmail.com>
2026-03-07 01:43:09 +0000

    add add_two function

--- main.py
+++ main.py
@@ -16,11 +16,17 @@ print("Goodbye!")
 # print hello world
 print("Hello, World!")
 
-# print good bye 
+# print good bye
 print("Goodbye!")
 
 # print new line
 print("I'm learning Github!")
 
-# print hello world 
+# print hello world
 print("Now I know what git and github is. Easy!")
+
+# new function
+def add_two(num):
+    return num + 2
+
+print(add_two(2), add_two(5))
